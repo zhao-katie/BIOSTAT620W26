1722d8e8e3058f2c7977f41bdd7a928a1c5c34e1
Katie Zhao <kazhao0903@gmail.com>
2026-02-12 20:29:18 -0500

    adding data

--- woah/test.R
+++ woah/test.R
@@ -1,3 +1,13 @@
 hello
 what's up'
 uerm
+
+url <- 'https://raw.githubusercontent.com/dmcable/BIOSTAT620W26/main/data/covid/population.csv'
+if (!file.exists("covid_processed.csv"))
+  download.file(
+    url = url,
+    destfile = "population.csv",
+    method   = "libcurl",
+    timeout  = 60
+  )
+population <- read.csv('population.csv')
